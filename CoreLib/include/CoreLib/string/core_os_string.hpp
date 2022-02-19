de3c8725a796f4e69c30eb5b5411a7e78f97da8d
Tiago Freire <tmiguelf@hotmail.com>
2022-02-19 22:34:28 +0100

    removed year from copyright

--- CoreLib/include/CoreLib/string/core_os_string.hpp
+++ CoreLib/include/CoreLib/string/core_os_string.hpp
@@ -3,7 +3,7 @@
 ///		String suitable for OS operations
 ///
 ///	\copyright
-///		Copyright (c) 2020 Tiago Miguel Oliveira Freire
+///		Copyright (c) Tiago Miguel Oliveira Freire
 ///
 ///		Permission is hereby granted, free of charge, to any person obtaining a copy
 ///		of this software and associated documentation files (the "Software"), to deal
--- version/include/version/version.h
+++ version/include/version/version.h
@@ -3,7 +3,7 @@
 ///		Used to prepare and format version information
 ///
 ///	\copyright
-///		Copyright (c) 2020 Tiago Miguel Oliveira Freire
+///		Copyright (c) Tiago Miguel Oliveira Freire
 ///
 ///		Permission is hereby granted, free of charge, to any person obtaining a copy
 ///		of this software and associated documentation files (the "Software"), to deal
