a9764fe0b304244400f697cbd6a0fdb5f5235584
Tiago Freire <tmiguelf@hotmail.com>
2023-12-01 19:19:24 +0100

    upgraded os string conversion

--- CoreLib/src/string/core_os_string.cpp
+++ CoreLib/src/string/core_os_string.cpp
@@ -31,85 +31,116 @@
 
 #include <span>
 
-#include <CoreLib/string/core_string_encoding.hpp>
-
 namespace core
 {
-
 #ifdef _WIN32
-static uintptr_t requiredConversionSize(std::u32string_view const p_string)
-{
-	uintptr_t count = 0;
-
-	for(const char32_t tchar : p_string)
+	static uintptr_t to_os_natural_size(std::u32string_view const p_string)
 	{
-		if(tchar > 0xFFFF)
+		uintptr_t count = 0;
+
+		for(const char32_t tchar : p_string)
 		{
-			if(tchar > 0x10FFFF)
+			if(tchar > 0xFFFF)
 			{
-				return 0;
+				if(tchar > 0x10FFFF)
+				{
+					return 0;
+				}
+				count += 2;
 			}
-			count += 2;
+			else
+			{
+				++count;
+			}
 		}
-		else
+		return count;
+	}
+
+	static uintptr_t from_os_natural_size(std::u16string_view const p_string)
+	{
+		uintptr_t count = 0;
+		const char16_t* pos = p_string.data();
+		const char16_t* const end = pos + p_string.size();
+
+		for(; pos < end; ++pos, ++count)
 		{
-			++count;
+			const char16_t tchar = *pos;
+			if((tchar & 0xFC00) == 0xD800)
+			{
+				const char16_t* pos_next = pos + 1;
+				if(pos_next < end && ((*pos_next) & 0xFC00) == 0xDC00)
+				{
+					++pos;
+				}
+			}
 		}
-	}
-	return count;
-}
-
-std::basic_string<os_char> to_os_natural_convert(std::u32string_view const p_string)
-{
-	const uintptr_t reqSize = requiredConversionSize(p_string);
-	if(reqSize == 0) return {};
-
-	std::basic_string<os_char> buff;
-	buff.resize(reqSize);
-
-	os_char* pivot = buff.data();
-
-	for(const char32_t tchar : p_string)
-	{
-		pivot += encode_UTF16(tchar, std::span<char16_t, 2>{reinterpret_cast<char16_t*>(pivot), 2});
+		return count;
 	}
 
-	return buff;
-}
-
-static char32_t extract_code_point(const char16_t*& p_input, const char16_t* const p_end)
-{
-	const char16_t* pivot = p_input;
-	if(*pivot > 0xD7FF && *pivot < 0xE000)
+	static uint8_t encode_UTF16(char32_t p_char, std::span<char16_t, 2> const p_output)
 	{
-		if((p_end - pivot < 1) ||
-			(*pivot & 0xFC00) != 0xD800 ||
-			(pivot[1] & 0xFC00) != 0xDC00)
+		if(p_char > 0x10FFFF)
 		{
-			return *p_input;
+			return 0;
+		}
+		if(p_char > 0xFFFF)
+		{
+			p_char -= 0x010000;
+			p_output[0] = static_cast<char16_t>(((p_char & 0xFFC00) >> 10) | 0xD800);
+			p_output[1] = static_cast<char16_t>((p_char & 0x003FF) | 0xDC00);
+			return 2;
+		}
+		p_output[0] = static_cast<char16_t>(p_char);
+		return 1;
+	}
+
+	std::basic_string<os_char> to_os_natural_convert(std::u32string_view const p_string)
+	{
+		const uintptr_t reqSize = to_os_natural_size(p_string);
+		if(reqSize == 0) return {};
+
+		std::wstring buff;
+		buff.resize(reqSize);
+
+		char16_t* pivot = reinterpret_cast<char16_t*>(buff.data());
+
+		for(const char32_t tchar : p_string)
+		{
+			pivot += encode_UTF16(tchar, std::span<char16_t, 2>{pivot, 2});
 		}
 
-		++p_input;
-		return (((char32_t{*pivot} & 0x03FF) << 10) | (char32_t{pivot[1]} & 0x03FF)) + 0x10000;
+		return buff;
 	}
-	return *p_input;
-}
 
-std::u32string from_os_natural_convert(std::basic_string_view<os_char> const p_string)
-{
-	std::u32string buff;
-	buff.reserve(p_string.size());
-
-	const char16_t* pos = reinterpret_cast<const char16_t*>(p_string.data());
-	const char16_t* const end = pos + p_string.size();
-	for(; pos < end; ++pos)
+	std::u32string from_os_natural_convert(std::basic_string_view<os_char> const p_string)
 	{
-		buff.push_back(extract_code_point(pos, end));
+		std::u16string_view const t_string{reinterpret_cast<const char16_t*>(p_string.data()), p_string.size()};
+		const uintptr_t reqSize = from_os_natural_size(t_string);
+		if(reqSize == 0) return {};
+
+		std::u32string buff;
+		buff.resize(reqSize);
+		char32_t* pivot = buff.data();
+
+		const char16_t* pos = t_string.data();
+		const char16_t* const end = pos + t_string.size();
+
+		for(; pos < end; ++pos)
+		{
+			const char16_t tchar = *pos;
+			if((tchar & 0xFC00) == 0xD800)
+			{
+				const char16_t* pos_next = pos + 1;
+				if(pos_next < end && ((*pos_next) & 0xFC00) == 0xDC00)
+				{
+					*(pivot++) = static_cast<char32_t>((((tchar & 0x03FF) << 10) | (*pos_next & 0x03FF)) + 0x10000);
+					++pos;
+					continue;
+				}
+			}
+			*(pivot++) = static_cast<char32_t>(tchar);
+		}
+		return buff;
 	}
-	return buff;
-}
 #endif
-
-
-
 } //namespace core
