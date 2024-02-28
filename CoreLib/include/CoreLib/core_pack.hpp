2cfd22e8be96a88ee958a1fdacfec302c3447f41
Tiago Freire <tmiguelf@hotmail.com>
2024-02-28 09:49:59 +0100

    Improved pack library

--- CoreLib/include/CoreLib/core_pack.hpp
+++ CoreLib/include/CoreLib/core_pack.hpp
@@ -27,10 +27,11 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <limits>
+#include <utility>
 
 namespace core
 {
-
 
 	template<typename ...>
 	struct pack {};
@@ -42,16 +43,48 @@ namespace core
 	template<typename ...T>
 	struct is_pack<pack<T...>> : public std::true_type{};
 
-
 	template<typename T>
 	constexpr bool is_pack_v = is_pack<T>::value;
-
 
 	template<typename T>
 	concept c_pack = is_pack<T>::value;
 
 
-	template<c_pack ...T>
+	//======== ======== pack utilities ======== ========
+
+	/// \brief Counts elements in a pack
+	template<c_pack>
+	struct pack_count {};
+
+	template<>
+	struct pack_count<pack<>> { static constexpr uintptr_t value = uintptr_t{0}; };
+
+	template<typename T1, typename ...T2>
+	struct pack_count<pack<T1, T2...>> { static constexpr uintptr_t value = uintptr_t{1} + pack_count<pack<T2...>>::value; };
+
+	template<c_pack T>
+	constexpr uintptr_t pack_count_v = pack_count<T>::value;
+
+	template<c_pack T>
+	constexpr bool is_pack_empty_v = (pack_count_v<T> == 0);
+
+
+	/// \brief Retrieves a specific element from a pack
+	template<c_pack, uintptr_t>
+	struct pack_get {};
+
+	template<typename T1, typename ...T2>
+	struct pack_get<pack<T1, T2...>, 0> { using type = T1; };
+
+	template<uintptr_t Index, typename T1, typename ...T2>
+	struct pack_get<pack<T1, T2...>, Index> { using type = typename pack_get<pack<T2...>, Index -1>::type; };
+
+	template<c_pack T, uintptr_t Index>
+	using pack_get_t = typename pack_get<T, Index>::type;
+
+
+	/// \brief Joins multiple packs toghether
+	template<c_pack ...>
 	struct pack_cat {};
 
 	template<>
@@ -63,37 +96,160 @@ namespace core
 	template<typename ...T1, typename ...T2, c_pack ...T3>
 	struct pack_cat<pack<T1...>, pack<T2...>, T3...> { using type = pack_cat<pack<T1..., T2...>, T3...>::type; };
 
-
 	template<c_pack ...T>
 	using pack_cat_t = pack_cat<T...>::type;
 
+	/// \brief Gets a sub pack interval
+	/// \tparam Pack_t - source pack
+	/// \tparam StartIndex - first index to keep
+	/// \tparam Size - size of new pack
+	template<c_pack Pack_t, uintptr_t StartIndex, uintptr_t Size = pack_count_v<Pack_t> - StartIndex>
+	struct sub_pack
+	{
+	private:
+
+		template <class IndexSequence>
+		struct conv_pack;
+
+		template <std::uintptr_t... indices>
+		struct conv_pack<std::index_sequence<indices...>> { using type = pack<pack_get_t<Pack_t, StartIndex + indices> ...>; };
+
+	public:
+		using type = conv_pack<std::make_index_sequence<Size>>::type;
+	};
+
+	template<c_pack Pack_t, uintptr_t StartIndex, uintptr_t Size = pack_count_v<Pack_t> - StartIndex>
+	using sub_pack_t = typename sub_pack<Pack_t, StartIndex, Size>::type;
 
 
-	template<c_pack>
-	struct pack_count {};
+	/// \brief Transforms all types in a pack in acordance to a transformation template
+	/// \tparam Pack_t - pack to transform
+	/// \tparam Transformation - Transformation template
+	template<c_pack Pack_t, template <typename> typename Transformation>
+	struct pack_transform
+	{
+	private:
+		static constexpr uintptr_t pack_size = pack_count_v<Pack_t>;
 
-	template<>
-	struct pack_count<pack<>> { static constexpr uintptr_t value = uintptr_t{0}; };
+		template <class IndexSequence>
+		struct conv_pack;
 
-	template<typename T1, typename ...T2>
-	struct pack_count<pack<T1, T2...>> { static constexpr uintptr_t value = uintptr_t{1} + pack_count<pack<T2...>>::value; };
+		template <std::uintptr_t... indices>
+		struct conv_pack<std::index_sequence<indices...>>
+		{
+			using type = pack<typename Transformation<pack_get_t<Pack_t, indices>>::type ...>;
+		};
+
+	public:
+		using type = typename conv_pack<std::make_index_sequence<pack_size>>::type;
+	};
+
+	template<c_pack Pack_t, template <typename> typename Transformation>
+	using pack_transform_t = typename pack_transform<Pack_t, Transformation>::type;
 
 
-	template<c_pack T>
-	constexpr uintptr_t pack_count_v = pack_count<T>::value;
+
+	static constexpr uintptr_t pack_npos = std::numeric_limits<uintptr_t>::max();
+	
+	/// \brief Gets the index of the type that satisfies the Predicate
+	/// \tparam Pack_t - pack to evaluate
+	/// \tparam Predicate - Checking condition
+	/// \tparam StartIndex - first index to start searching
+	/// \tparam EndIndex - 1 past last index to check
+	template<c_pack Pack_t, template <typename> typename Predicate,
+		uintptr_t StartIndex = 0, uintptr_t EndIndex = pack_count_v<Pack_t>>
+	struct pack_find
+	{
+	private:
+		template<uintptr_t Index = StartIndex>
+		static consteval uintptr_t find()
+		{
+			if constexpr (Index < EndIndex)
+			{
+				if constexpr (Predicate<pack_get_t<Pack_t, Index>>::value)
+				{
+					return Index;
+				}
+				else
+				{
+					return find<Index + 1>();
+				}
+			}
+			else
+			{
+				return pack_npos;
+			}
+		}
+
+	public:
+		static constexpr uintptr_t value = find();
+	};
+
+	template<c_pack Pack_t, template <typename> typename Predicate,
+		uintptr_t StartIndex = 0, uintptr_t EndIndex = pack_count_v<Pack_t>>
+	static constexpr uintptr_t pack_find_v = pack_find<Pack_t, Predicate, StartIndex, EndIndex>::value;
 
 
+	/// \brief Checks if any of the types in the pack satisfies the Predicate
+	/// \tparam Pack_t - pack to evaluate
+	/// \tparam Predicate - Checking condition
+	/// \tparam StartIndex - first index to start checking
+	/// \tparam EndIndex - 1 past last index to check
+	template<c_pack Pack_t, template <typename> typename Predicate,
+		uintptr_t StartIndex = 0, uintptr_t EndIndex = pack_count_v<Pack_t>>
+	struct pack_contains
+	{
+	private:
+		template<uintptr_t Index = StartIndex>
+		static constexpr bool check()
+		{
+			if constexpr (Index < EndIndex)
+			{
+				if constexpr (Predicate<pack_get_t<Pack_t, Index>>::value)
+				{
+					return true;
+				}
+				else
+				{
+					return check<Index + 1>();
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
 
-	template<uintptr_t, c_pack ...>
-	struct pack_get {};
+	public:
+		static constexpr bool value = check();
+	};
 
-	template<typename T1, typename ...T2>
-	struct pack_get<0, pack<T1, T2...>> { using type = T1; };
+	template<c_pack Pack_t, template <typename> typename Predicate,
+		uintptr_t StartIndex = 0, uintptr_t EndIndex = pack_count_v<Pack_t>>
+	static constexpr bool pack_contains_v = pack_contains<Pack_t, Predicate, StartIndex, EndIndex>::value;
 
-	template<uintptr_t Index, typename T1, typename ...T2>
-	struct pack_get<Index, pack<T1, T2...>> { using type = pack_get<Index -1,  pack<T2...>>::type; };
 
-	template<uintptr_t Index, c_pack ...T>
-	using pack_get_t = pack_get<Index, T...>::type;
+	/// \brief Swap 2 elements in a pack
+	template <c_pack Pack_t, std::uintptr_t I, std::uintptr_t J>
+	struct pack_element_swap
+	{
+	private:
+		static constexpr uintptr_t pack_size = pack_count_v<Pack_t>;
+
+		template <class IndexSequence>
+		struct pack_element_swap_impl;
+
+		template <std::uintptr_t... indices>
+		struct pack_element_swap_impl<std::index_sequence<indices...>>
+		{
+			using type = pack<pack_get_t<Pack_t, (indices != I && indices != J) ? indices : ((indices == I) ? J : I)>...>;
+		};
+
+	public:
+		using type = typename pack_element_swap_impl<std::make_index_sequence<pack_size>>::type;
+	};
+
+	template <c_pack Pack_t, std::uintptr_t I, std::uintptr_t J>
+	using pack_element_swap_t = typename pack_element_swap<Pack_t, I, J>::type;
 
 } //namespace core
---
+++ CoreLib/tests/src/pack_test.cpp
@@ -0,0 +1,134 @@
+
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include <CoreLib/core_pack.hpp>
+
+
+TEST(core_pack, pack_equivalence)
+{
+	static_assert(std::is_same_v<core::pack<>, core::pack<>>);
+	static_assert(!std::is_same_v<core::pack<>, core::pack<void>>);
+	static_assert(!std::is_same_v<core::pack<void>, core::pack<void*>>);
+	static_assert(!std::is_same_v<core::pack<uint32_t>, core::pack<uint32_t const>>);
+	static_assert(!std::is_same_v<core::pack<uint32_t>, core::pack<uint32_t&>>);
+	static_assert(std::is_same_v<core::pack<uint32_t, float, void*>, core::pack<uint32_t, float, void*>>);
+	static_assert(!std::is_same_v<core::pack<uint32_t, float, void*>, core::pack<uint32_t, float>>);
+	static_assert(!std::is_same_v<core::pack<uint32_t, float>, core::pack<float, uint32_t>>);
+}
+
+TEST(core_pack, pack_count)
+{
+	static_assert(core::pack_count_v<core::pack<>> == 0);
+	static_assert(core::pack_count_v<core::pack<void>> == 1);
+	static_assert(core::pack_count_v<core::pack<uint32_t, uint32_t&, uint32_t*, const uint32_t>> == 4);
+	static_assert(core::pack_count_v<core::pack<float, double, void*, core::pack<float, double>, void*, void>> == 6);
+}
+
+TEST(core_pack, pack_get)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double, uint32_t&>;
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 0>, uint32_t>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 1>, float>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 2>, void*>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 3>, void>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 4>, const int64_t>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 5>, double>);
+	static_assert(std::is_same_v<core::pack_get_t<test_t, 6>, uint32_t&>);
+}
+
+TEST(core_pack, pack_cat)
+{
+	using test_t1 = core::pack<>;
+	using test_t2 = core::pack<void>;
+	using test_t3 = core::pack<uint32_t, float>;
+	using test_t4 = core::pack<const int32_t&, double>;
+	using test_t5 = core::pack<void*, core::pack<uint64_t>>;
+
+	static_assert(std::is_same_v<core::pack_cat_t<test_t1>, core::pack<>>);
+	static_assert(std::is_same_v<core::pack_cat_t<test_t3, test_t4>, core::pack<uint32_t, float, const int32_t&, double>>);
+	static_assert(std::is_same_v<core::pack_cat_t<test_t1, test_t5, test_t2>, core::pack<void*, core::pack<uint64_t>, void>>);
+}
+
+TEST(core_pack, sub_pack)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double, uint32_t&>;
+
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 0>, test_t>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 3>, core::pack<void, const int64_t, double, uint32_t&>>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 3, 2>, core::pack<void, const int64_t>>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 4, 3>, core::pack<const int64_t, double, uint32_t&>>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 0, 3>, core::pack<uint32_t, float, void*>>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 0, 0>, core::pack<>>);
+	static_assert(std::is_same_v<core::sub_pack_t<test_t, 6, 0>, core::pack<>>);
+	static_assert(std::is_same_v<core::sub_pack_t<core::pack<>, 0, 0>, core::pack<>>);
+}
+
+
+template<typename T>
+struct transform_test_t { using type = T; };
+
+template<typename T> requires std::is_integral_v<T>
+struct transform_test_t<T>
+{
+	using type = std::conditional_t<std::is_signed_v<T>, std::make_unsigned_t<T>, std::make_signed_t<T>>;
+};
+
+template<typename T> requires std::is_floating_point_v<T>
+struct transform_test_t<T>
+{
+	using type = T*;
+};
+
+TEST(core_pack, pack_transform)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double, uint32_t&>;
+
+	static_assert(std::is_same_v<core::pack_transform_t<test_t, transform_test_t>, core::pack<int32_t, float*, void*, void, const uint64_t, double*, uint32_t&>>);
+}
+
+
+template<typename>
+struct find_test_t: std::false_type {};
+
+template<typename T> requires std::is_pointer_v<T>
+struct find_test_t<T>: std::true_type {};
+
+TEST(core_pack, pack_find)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double*, uint32_t&, int8_t>;
+	static_assert(core::pack_find_v<test_t, find_test_t> == 2);
+	static_assert(core::pack_find_v<test_t, find_test_t, 3> == 5);
+	static_assert(core::pack_find_v<test_t, find_test_t, 3, 5> == core::pack_npos);
+	static_assert(core::pack_find_v<test_t, find_test_t, 6> == core::pack_npos);
+	static_assert(core::pack_find_v<test_t, find_test_t, 5> == 5);
+	static_assert(core::pack_find_v<core::pack<>, find_test_t> == core::pack_npos);
+}
+
+
+TEST(core_pack, pack_contains)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double*, uint32_t&, int8_t>;
+	static_assert(core::pack_contains_v<test_t, find_test_t> == true);
+	static_assert(core::pack_contains_v<test_t, find_test_t, 3> == true);
+	static_assert(core::pack_contains_v<test_t, find_test_t, 3, 5> == false);
+	static_assert(core::pack_contains_v<test_t, find_test_t, 6> == false);
+	static_assert(core::pack_contains_v<test_t, find_test_t, 5> == true);
+	static_assert(core::pack_contains_v<core::pack<>, find_test_t> == false);
+}
+
+
+TEST(core_pack, pack_element_swap)
+{
+	using test_t = core::pack<uint32_t, float, void*, void, const int64_t, double*, uint32_t&, int8_t>;
+
+	static_assert(std::is_same_v<core::pack_element_swap_t<core::pack<>, 0, 0>, core::pack<>>);
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 0, 0>, test_t>);
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 1, 1>, test_t>);
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 2, 2>, test_t>);
+
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 1, 2>, core::pack<uint32_t, void*, float, void, const int64_t, double*, uint32_t&, int8_t>>);
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 7, 3>, core::pack<uint32_t, float, void*, int8_t, const int64_t, double*, uint32_t&, void>>);
+	static_assert(std::is_same_v<core::pack_element_swap_t<test_t, 4, 6>, core::pack<uint32_t, float, void*, void, uint32_t&, double*, const int64_t, int8_t>>);
+}
