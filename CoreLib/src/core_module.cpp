f7e2fca3a07e8965937fd54e07e95a2780eaf569
Tiago Freire <67021355+tmiguelf@users.noreply.github.com>
2024-03-31 20:05:48 +0200

    Added ability to retrieve current module name (#47)

--- CoreLib/src/core_module.cpp
+++ CoreLib/src/core_module.cpp
@@ -24,52 +24,99 @@
 //======== ======== ======== ======== ======== ======== ======== ========
 
 #include <CoreLib/core_module.hpp>
-
-
+#include <CoreLib/core_os.hpp>
 #ifdef _WIN32
 #include <Windows.h>
+#include <filesystem>
+#include <array>
 #else
 #include <dlfcn.h>
+#include <link.h>
 #endif
 
 namespace core
 {
 
+	namespace
+	{
+		struct ModuleDataRetriever
+		{
+		public:
 #ifdef _WIN32
-
-static void const* real_get_current_module_base()
-{
-	HMODULE mod_addr;
-	if(GetModuleHandleExW(
-		GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-		reinterpret_cast<wchar_t*>(get_current_module_base), &mod_addr))
-	{
-		return mod_addr;
-	}
-
-	return nullptr;
-}
-
+			ModuleDataRetriever()
+			{
+				HMODULE mod_addr;
+				if(GetModuleHandleExW(
+					GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+					reinterpret_cast<wchar_t*>(get_current_module_base), &mod_addr))
+				{
+					module_addr = mod_addr;
+					std::filesystem::path temp;
+					{
+						std::array<wchar_t, 32767> data;
+						DWORD const ret_size = GetModuleFileNameW(mod_addr, data.data(), static_cast<DWORD>(data.size()));
+						if(ret_size && (ret_size < data.size()))
+						{
+							temp = std::filesystem::path{data.data(), data.data() + ret_size};
+						}
+						else
+						{
+							return;
+						}
+					}
+					module_name = temp.lexically_normal();
+					short_name = module_name.filename();
+				}
+				else
+				{
+					module_addr = nullptr;
+				}
+			}
 #else
+			ModuleDataRetriever()
+			{
+				Dl_info t_info;
 
-static void const* real_get_current_module_base()
-{
-	Dl_info t_info;
-	void const * const addr = reinterpret_cast<void const * const>(get_current_module_base);
-	if(dladdr(addr, &t_info) && (t_info.dli_fbase < addr))
-	{
-		return t_info.dli_fbase;
-	}
-	return nullptr;
-}
+				void const * const addr = reinterpret_cast<void const * const>(get_current_module_base);
+				if(dladdr(addr, &t_info) && (t_info.dli_fbase < addr))
+				{
+					module_addr = t_info.dli_fbase;
+					std::filesystem::path temp{t_info.dli_fname};
+					module_name = temp.lexically_normal();
+					short_name = module_name.filename();
+				}
+				else
+				{
+					module_addr = nullptr;
+				}
 
+			}
 #endif
 
-static void const* const g_module_addr = real_get_current_module_base();
+		public:
+			void const* module_addr;
+			std::filesystem::path module_name;
+			std::filesystem::path short_name;
+		};
+
+	} //namespace
+
+
+static ModuleDataRetriever const g_module_data;
 
 void const* get_current_module_base()
 {
-	return g_module_addr;
+	return g_module_data.module_addr;
 }
 
-} //namespace core
+os_string_view get_current_module_name()
+{
+	return g_module_data.module_name.native();
+}
+
+os_string_view get_current_module_short_name()
+{
+	return g_module_data.short_name.native();
+}
+
+}
